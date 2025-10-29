02d21b6fc0e162b22c5927547ffa64193b8b44ad
Arthur Fernando <arthurfernando4305@gmail.com>
2025-10-29 12:51:50 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.getElementById("btn-menu");
-const nav = document.getElementById("menu");
-
-btnMenu.addEventListener("click", () => {
-  nav.classList.toggle("ativo");
-});
